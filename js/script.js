590a8f27cd7a307db0265e6e3534e1ee48da779a
Mehrsa <mhrs.project22@gmail.com>
2023-11-16 00:25:59 +0330

    show alert message

--- js/script.js
+++ js/script.js
@@ -1,8 +1,23 @@
 const taskInput = document.getElementById("task-input");
 const dateInput = document.getElementById("date-task");
 const addButton = document.getElementById("add-button");
+const alertMessage = document.getElementById("alert-message");
 
 let todos = [];
+
+// show alert message
+const showAlert = (message, type) => {
+    alertMessage.innerHTML = "";
+    const alert = document.createElement("p");
+    alert.innerText = message;
+    alert.classList.add("alert");
+    alert.classList.add(`alert-${type}`);
+    alertMessage.append(alert);
+
+    setTimeout(() => {
+        alert.style.display = "none";
+    }, 2000);
+};
 
 // get todo values
 const addHandler = () => {
@@ -13,13 +28,13 @@ const addHandler = () => {
         date,
         completed: false,
     };
-
     if (task) {
         todos.push(todo);
         taskInput.value = "";
         dateInput.value = "";
+        showAlert("todo added successfully", "success");
     } else {
-        alert("pls enter a task");
+        showAlert("please enter a todo!", "error");
     }
 };
 
